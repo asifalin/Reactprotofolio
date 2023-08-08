09453465bd085bb644ed657a201e138b7dc584aa
asifali12345678 <asifnagri55@gmail.com>
2023-08-08 14:03:31 +0500

    headersec

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,3 +1,4 @@
+import Header from "./Component/HeaderSec/Header";
 import Navbar from "./Component/NavbarSec/Navbar";
 
 export default function App() {
@@ -7,6 +8,10 @@ export default function App() {
    <section className='bg-[aliceblue] container  max-w-[1800px]'>
 <div>
 <Navbar/>
+</div>
+
+<div>
+  <Header/>
 </div>
    </section>
   )
